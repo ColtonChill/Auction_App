741244d012dcb2ca3c32cde0b2b0a0ebbe4e9db4
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2020-02-28 15:15:05 -0700

    Move router to it's own folder.

--- project/api/src/Index.ts
+++ project/api/src/Index.ts
@@ -1,13 +1,12 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 import Koa from 'koa';
-import Router from 'koa-router';
 import logger from './services/Logger';
 import { migrate } from './services/Database'; 
 import { Server } from 'http';
+import indexRouter from './routes/Routes';
 
 const app : Koa = new Koa();
-const router : Router = new Router();
 
 const port = process.env.PORT || 3000;
 
@@ -16,7 +15,7 @@ router.get('/', async ctx => {
     ctx.status = 200;
 })
 
-app.use(router.routes()).use(router.allowedMethods());
+app.use(indexRouter.routes()).use(indexRouter.allowedMethods());
 
 logger.info("Migrating database to latest version.");
 migrate();
