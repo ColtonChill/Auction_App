11ac89411b1cc1a3989c42577605872e503ff2c3
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2020-02-28 13:04:14 -0700

    Add example test.

--- project/api/src/Index.ts
+++ project/api/src/Index.ts
@@ -4,14 +4,16 @@ import Koa from 'koa';
 import Router from 'koa-router';
 import logger from './services/Logger';
 import { migrate } from './services/Database'; 
+import { Server } from 'http';
 
-const app = new Koa();
-const router = new Router();
+const app : Koa = new Koa();
+const router : Router = new Router();
 
 const port = process.env.PORT || 3000;
 
 router.get('/', async ctx => {
-    ctx.body = "Hello!";
+    ctx.body = {status: "OK"};
+    ctx.status = 200;
 })
 
 app.use(router.routes()).use(router.allowedMethods());
@@ -20,6 +22,6 @@ logger.info("Migrating database to latest version.");
 migrate();
 logger.info("Done!");
 
-app.listen(port, async () => {
+export const server : Server = app.listen(port, async () => {
     logger.info(`Server running on port ${port}.`)
 });
