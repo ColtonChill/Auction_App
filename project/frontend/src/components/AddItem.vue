b11e05d842352b8eb1117a24511b4e9763cdd0af
Jace Longhurst <jtlonghurst@gmail.com>
2020-03-24 23:54:06 -0600

    Tried to add some style

--- project/frontend/src/components/AddItem.vue
+++ project/frontend/src/components/AddItem.vue
@@ -1,20 +1,30 @@
 <template>
     <div>
-        <!-- Page to add an item on if you are an admin --> 
-        <h1>Add an Item</h1>
-        <form action = '/api/v1/item/'>
-            <label for = "name">Item Name: </label>
-            <input id = "name">
-            
-            <label for = "description">Description: </label>
-            <input id = "description">
-
-            <label for = "picture">Picture: </label>
-            <input type = 'file' accept = "image/*" id="picture">  
-
-            <label for = "sBid">Starting Bid</label>
-            <input id = "sBid">
-        </form>
-        <button type="submit" action = "POST">Add Item </button>
+        <!-- Page to add an item on if you are an admin-->
+        <div class = "shadowlg rounded object-center">
+            <h1 class = "max-w-mdfont-bold text-center text-gray-500
+             underline text-3xl">
+             Add an Item</h1>
+            <form action = '/api/v1/item/'>
+                <label for = "name">Item Name: </label>
+                <input id = "name">
+                <br>
+                <label for = "description">Description: </label>
+                <input id = "description">
+                <br>
+                <label for = "picture">Picture: </label>
+                <input type = 'file' accept = "image/*" id="picture">
+                <br>
+                <label for = "sBid">Starting Bid</label>
+                <input id = "sBid">
+            </form>
+            <button type="submit" action = "POST">Add Item </button>
+        </div>
     </div>
 </template>
+
+<script lang="ts">
+export default {
+  name: 'AddItem',
+};
+</script>
--- project/frontend/src/router/index.ts
+++ project/frontend/src/router/index.ts
@@ -3,6 +3,7 @@ import Router from 'vue-router';
 import LandingPage from '@/components/LandingPage.vue';
 import RegisterPage from '@/components/RegisterPage.vue';
 import LoginPage from '@/components/LoginPage.vue';
+import AddItem from '@/components/AddItem.vue';
 
 
 Vue.use(Router);
@@ -24,6 +25,11 @@ export default new Router({
       path: '/login',
       name: 'LoginPage',
       component: LoginPage,
+    },
+    {
+      path: '/addItem',
+      name: 'AddItem',
+      component: AddItem,
     },
   ],
 });
