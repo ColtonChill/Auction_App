ccfc56d10c1422d0199c3c6de44250ec03e1f448
Connor Geslin <connorgeslin23@gmail.com>
2020-03-25 21:00:57 -0600

    stuff

--- project/frontend/src/components/ItemWinner.vue
+++ project/frontend/src/components/ItemWinner.vue
@@ -1,12 +1,12 @@
 <template>
   <div style="width: 600px">
     <table>
-  <tr v-for"item in items" :key="item.id">
-    <td>{{ item.name }}</td>
-    <td>{{ item.winningBid.amount }}</td>
-    <td>{{ item.winningBid.user.firstName }} {{ item.winningBid.user.lastName }}</td>
-  </tr>
-<table>
+      <tr v-for"item in items" :key="item.id">
+        <td>{{ item.name }}</td>
+        <td>{{ item.winningBid.amount }}</td>
+        <td>{{ item.winningBid.user.firstName }} {{ item.winningBid.user.lastName }}</td>
+      </tr>
+    <table>
   </div>
 </template>
 
@@ -16,10 +16,10 @@ export default {
   data() {
     return {
       items: [
-        { id: 40, price: '501', winner: 'thedude' },
-        { id: 21, price: '23', winner: 'me' },
-        { id: 89, price: '24', winner: 'you' },
-        { id: 38, price: '454', winner: 'her' },
+        { id: 40, price: '501', winnerFirstname: 'thedude', winnerLastName: 'stuff' },
+        { id: 21, price: '23', winnerFirstname: 'me', winnerLastName: 'stuff'},
+        { id: 89, price: '24', winnerFirstname: 'you', winnerLastName: 'stuff'},
+        { id: 38, price: '454', winnerFirstname: 'her', winnerLastName: 'stuff'},
       ],
     };
   },
