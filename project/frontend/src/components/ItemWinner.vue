e3f1213422beb03c63c27c3c12a67f336cd0f633
Connor Geslin <connorgeslin23@gmail.com>
2020-03-25 21:04:31 -0600

    stuff

--- project/frontend/src/components/ItemWinner.vue
+++ project/frontend/src/components/ItemWinner.vue
@@ -4,7 +4,8 @@
       <tr v-for"item in items" :key="item.id">
         <td>{{ item.name }}</td>
         <td>{{ item.winningBid.amount }}</td>
-        <td>{{ item.winningBid.user.firstName }} {{ item.winningBid.user.lastName }}</td>
+        <td>{{ item.winningBid.user.firstName }}</td>
+        <td>{{ item.winningBid.user.lastName }}</td>
       </tr>
     <table>
   </div>
@@ -16,10 +17,9 @@ export default {
   data() {
     return {
       items: [
-        { id: 40, price: '501', winnerFirstname: 'thedude', winnerLastName: 'stuff' },
-        { id: 21, price: '23', winnerFirstname: 'me', winnerLastName: 'stuff'},
-        { id: 89, price: '24', winnerFirstname: 'you', winnerLastName: 'stuff'},
-        { id: 38, price: '454', winnerFirstname: 'her', winnerLastName: 'stuff'},
+        { id: 40, price: 501, winningBid: { amount: 5, user: { firstName: 'Hunter', lastName: 'Henrichsen', email: 'hunter.henrichsen@gmail.com' }}, 
+        name: 'Book of Uselessness', description: 'A completely useless book.' }
+        
       ],
     };
   },
