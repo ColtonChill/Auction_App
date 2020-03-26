332c0b476e917110c3a3649f1289b9f37eb0d737
Connor Geslin <connorgeslin23@gmail.com>
2020-03-25 19:30:57 -0600

    tables

---
+++ project/frontend/src/components/BidderCommitment.vue
@@ -0,0 +1,21 @@
+<template>
+  <div style="width: 600px">
+   
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'BidderCommitment',  
+    data() {
+      return {
+        items: [
+          { user: joe, totalCommitment: '$501'},
+          { user: momma, totalCommitment: '$10' },
+          { user: thatguy, totalCommitment: '$24',},
+          { user: OhyouKnow, totalCommitment: '454',}
+        ]
+      }
+    }
+  }
+</script>
---
+++ project/frontend/src/components/ItemWinner.vue
@@ -0,0 +1,21 @@
+<template>
+  <div style="width: 600px">
+   
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ItemWinnerList',  
+    data() {
+      return {
+        items: [
+          { item: 40, price: '$501', winner: 'thedude' },
+          { item: 21, price: '$23', winner: 'me' },
+          { item: 89, price: '$24', winner: 'you' },
+          { item: 38, price: '454', winner: 'her' }
+        ]
+      }
+    }
+  }
+</script>
