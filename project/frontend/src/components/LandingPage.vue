72a5a3447696590a08cb06530fad0eb87f6b8c5e
Connor Geslin <connorgeslin23@gmail.com>
2020-03-25 20:27:11 -0600

    link

--- project/frontend/src/components/LandingPage.vue
+++ project/frontend/src/components/LandingPage.vue
@@ -2,13 +2,14 @@
       <div id="landing">
     <p>
       <!-- <router-link to="/register">Go to RegisterPage</router-link>
-      <router-link to="/ItemWinner">Go to ItemWinner</router-link>
       <router-link to="/login">Go to Login!</router-link> -->
     </p>
     <h1 class="font-title text-4xl text-semibold text-center pt-4 text-darkBlue">
       Welcome to Awection!</h1>
     <h2 class="text-xl text-semibold text-center pt-6 pb-6 text-midBlue">
       Select an auction from below:</h2>
+    <h3 class="font-title text-4xl text-semibold text-center pt-4 text-darkBlue">
+      <!--router-link to="/ItemWinner"> Go to ItemWinner</router-link></h3>
 
     <AuctionList/>
   </div>
