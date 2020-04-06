6ecfddfd1b6100b4b81d9fa028f06ba24f0b59ec
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2020-04-05 20:19:45 -0600

    Fix issue?

--- project/frontend/src/App.vue
+++ project/frontend/src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <div id="app">
     <ul class="flex bg-darkBlue h-10">
-  <li class="mx-6 mt-2 flex-grow-0">
-    <router-link to="/" class="text-white hover:text-blue-400">Home</router-link>
-  </li>
-  <li class="flex-grow-1">
-  </li>
-  <li class="mx-6 mt-2 flex-grow-0">
-    <router-link to="/login" class="text-white hover:text-blue-400">Profile </router-link>
-  </li>
-</ul>
+      <li class="mx-6 mt-2 flex-grow-0">
+        <router-link to="/" class="text-white hover:text-blue-400">Home</router-link>
+      </li>
+        <li class="flex-grow-1">
+      </li>
+      <li class="mx-6 mt-2 flex-grow-0">
+        <router-link to="/login" class="text-white hover:text-blue-400">Profile </router-link>
+      </li>
+    </ul>
     <!-- <router-link to="/register">Register    </router-link>
     <router-link to="/login">Login</router-link> -->
     <router-view></router-view>
