3527dfaf3a5eccef86a7bb4fbf9a722be41ee714
Connor Geslin <connorgeslin23@gmail.com>
2020-04-07 17:23:38 -0600

    Admin page

--- project/frontend/src/components/AdminPage.vue
+++ project/frontend/src/components/AdminPage.vue
@@ -2,18 +2,18 @@
 
 <!-- Admin form...  -->
 <div class="flex col">
-<div class="max-w-lg w-300 rounded overflow-hidden shadow-lg my-2 object-center mx-auto">
+<div class="w-full rounded overflow-hidden shadow-lg my-2 object-center mx-auto">
 
 <form class="mx-12">
 
 <div class="md:flex md:items-center mb-6 mt-4">
-    <div class="w-20">
+    <div class="w-30 flex-0">
       <label class="block text-gray-600 font-bold
-      md:text-right mb-1 md:mb-0 pr-4" for="auction-name">
+      lg:text-left mb-1 md:mb-0 pr-4" for="auction-name">
         Auction Name
       </label>
     </div>
-    <div class="">
+    <div class="flex-1">
       <input v-model="name" class="bg-gray-200 appearance-none border-2
       border-gray-200 rounded w-full py-2 px-4 text-gray-700
       leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
@@ -22,28 +22,28 @@
   </div>
 
   <div class="md:flex md:items-center mb-6 mt-4">
-    <div class="w-20">
+    <div class="w-30 flex-0">
       <label class="block text-gray-600 font-bold
-      md:text-right mb-1 md:mb-0 pr-4" for="description">
+      md:text-left mb-1 md:mb-0 pr-4" for="description">
         Description
       </label>
     </div>
-    <div class="">
+    <div class="flex-1">
       <input v-model="description" class="bg-gray-200 appearance-none border-2
       border-gray-200 rounded w-full py-2 px-4 text-gray-700
       leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
-      id="description" type="text" value="">
+      id="description" type="text">
     </div>
   </div>
 
   <div class="md:flex md:items-center mb-6 mt-4">
-    <div class="w-20">
+    <div class="w-30">
       <label class="block text-gray-600 font-bold
-      md:text-right mb-1 md:mb-0 pr-4" for="location">
+      md:text-left mb-1 md:mb-0 pr-4" for="location">
         Location
       </label>
     </div>
-    <div class="">
+    <div class="w-full">
       <input v-model="location" class="bg-gray-200 appearance-none border-2
       border-gray-200 rounded w-full py-2 px-4 text-gray-700
       leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
@@ -52,21 +52,24 @@
   </div>
 
   <div class="md:flex md:items-center mb-6 mt-4">
+    <div class="w-30">
+      <label class="block text-gray-600 font-bold mb-1 md:mb-0 pr-4" for="public">
+      Public
+      </label>
+    </div>
     <div class="">
-      <label class="block text-gray-600 font-bold mb-1 md:mb-0 pr-4" for="public"> Public </label>
       <input v-model="checkbox" id="public" type="checkbox">
-
     </div>
   </div>
 
   <div class="md:flex md:items-center mb-6 mt-4">
-    <div class="w-20">
+    <div class="w-30">
       <label class="block text-gray-600 font-bold
       md:text-right mb-1 md:mb-0 pr-4" for="url">
         URL
       </label>
     </div>
-    <div class="">
+    <div class="w-full">
       <input v-model="url" class="bg-gray-200 appearance-none border-2
       border-gray-200 rounded w-full py-2 px-4 text-gray-700
       leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
@@ -74,13 +77,29 @@
     </div>
   </div>
   <div class="md:flex md:items-center">
-    <div class="md:w-1/3"></div>
+    <div class="w-30">
+      <label class="block text-gray-600 font-bold
+      md:text-right mb-1 md:mb-0 pr-4" for="code">
+        Join Code
+      </label>
+    </div>
+    <div class="w-full">
+      <router-link to="/">
+      <button type="button" class="center shadow bg-blue-400 hover:bg-blue-600
+      focus:shadow-outline focus:outline-none text-white font-bold
+      py-2 px-8 rounded mb-4" @click="handleReg('api/v1/auth/register', first, last, eml, pass)"
+      >Regen Code</button>
+      </router-link>
+    </div>
+  </div>
+  <div class="md:flex md:items-center">
+    <div class="md:w-1/2"></div>
     <div class="md:w-2/3">
       <router-link to="/">
       <button type="button" class="center shadow bg-blue-400 hover:bg-blue-600
       focus:shadow-outline focus:outline-none text-white font-bold
-      py-2 px-4 rounded mb-4" @click="handleReg('api/v1/auth/register', first, last, eml, pass)"
-      >Sign Up</button>
+      py-2 px-4 rounded mb-4" @click="handleSave('api/v1/auth/register', first, last, eml, pass)"
+      >Save</button>
       </router-link>
     </div>
   </div>
@@ -104,7 +123,7 @@ export default {
     };
   },
   methods: {
-    async handleReg(url = '', first = '', last = '', eml = '', pass = '') {
+    async handleSave(url = '', first = '', last = '', eml = '', pass = '') {
       /* eslint-disable */
       console.log(first, last, eml, pass);
       const data = {
