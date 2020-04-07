22a636c114a8b37b16aefbdb51818c6e646e8848
Connor Geslin <connorgeslin23@gmail.com>
2020-04-07 15:40:59 -0600

    Checkbox functionality

--- project/frontend/src/components/AdminPage.vue
+++ project/frontend/src/components/AdminPage.vue
@@ -58,10 +58,7 @@
       </label>
     </div>
     <div class="">
-      <input v-model="checkbox"  class="bg-gray-200 appearance-none border-2
-      border-gray-200 rounded w-full py-2 px-4 text-gray-700
-      leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
-      id="checkbox" type="checkbox" value="">
+      <input v-model="checkbox" id="checkbox" type="checkbox" value="">
     </div>
   </div>
 
