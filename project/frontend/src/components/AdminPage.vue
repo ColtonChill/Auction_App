325ec8adcca4ef9233db4fb465dcafe3bd13b32e
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2020-04-07 17:35:14 -0600

    Styling tweaks.

--- project/frontend/src/components/AdminPage.vue
+++ project/frontend/src/components/AdminPage.vue
@@ -2,12 +2,12 @@
 
 <!-- Admin form...  -->
 <div class="flex col">
-<div class="w-full rounded overflow-hidden shadow-lg my-2 object-center mx-auto">
+<div class="md:w-3/4 sm:w-full rounded overflow-hidden shadow-lg my-2 object-center mx-auto mt-12">
 
 <form class="mx-12">
 
 <div class="md:flex md:items-center mb-6 mt-4">
-    <div class="w-30 flex-0">
+    <div class="w-32 flex-0">
       <label class="block text-gray-600 font-bold
       lg:text-left mb-1 md:mb-0 pr-4" for="auction-name">
         Auction Name
@@ -22,7 +22,7 @@
   </div>
 
   <div class="md:flex md:items-center mb-6 mt-4">
-    <div class="w-30 flex-0">
+    <div class="w-32 flex-0">
       <label class="block text-gray-600 font-bold
       md:text-left mb-1 md:mb-0 pr-4" for="description">
         Description
@@ -37,7 +37,7 @@
   </div>
 
   <div class="md:flex md:items-center mb-6 mt-4">
-    <div class="w-30">
+    <div class="w-32">
       <label class="block text-gray-600 font-bold
       md:text-left mb-1 md:mb-0 pr-4" for="location">
         Location
@@ -52,24 +52,24 @@
   </div>
 
   <div class="md:flex md:items-center mb-6 mt-4">
-    <div class="w-30">
+    <div class="w-32 flex-0">
       <label class="block text-gray-600 font-bold mb-1 md:mb-0 pr-4" for="public">
       Public
       </label>
     </div>
-    <div class="">
+    <div class="flex-1">
       <input v-model="checkbox" id="public" type="checkbox">
     </div>
   </div>
 
   <div class="md:flex md:items-center mb-6 mt-4">
-    <div class="w-30">
+    <div class="w-32 flex-0">
       <label class="block text-gray-600 font-bold
-      md:text-right mb-1 md:mb-0 pr-4" for="url">
+       mb-1 md:mb-0 pr-4" for="url">
         URL
       </label>
     </div>
-    <div class="w-full">
+    <div class="w-full flex-1">
       <input v-model="url" class="bg-gray-200 appearance-none border-2
       border-gray-200 rounded w-full py-2 px-4 text-gray-700
       leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
@@ -77,13 +77,13 @@
     </div>
   </div>
   <div class="md:flex md:items-center">
-    <div class="w-30">
+    <div class="w-32 flex-0">
       <label class="block text-gray-600 font-bold
-      md:text-right mb-1 md:mb-0 pr-4" for="code">
+      mb-1 md:mb-0 pr-4" for="code">
         Join Code
       </label>
     </div>
-    <div class="w-full">
+    <div class="w-full flex-1">
       <router-link to="/">
       <button type="button" class="center shadow bg-blue-400 hover:bg-blue-600
       focus:shadow-outline focus:outline-none text-white font-bold
@@ -98,7 +98,7 @@
       <router-link to="/">
       <button type="button" class="center shadow bg-blue-400 hover:bg-blue-600
       focus:shadow-outline focus:outline-none text-white font-bold
-      py-2 px-4 rounded mb-4" @click="handleSave('api/v1/auth/register', first, last, eml, pass)"
+      py-2 px-4 rounded mb-4"
       >Save</button>
       </router-link>
     </div>
