6a5a2a3fbf97ad606d419dfa15a19d79d2a52bff
Connor Geslin <connorgeslin23@gmail.com>
2020-04-07 15:57:42 -0600

    checkbox please work

--- project/frontend/src/components/AdminPage.vue
+++ project/frontend/src/components/AdminPage.vue
@@ -53,7 +53,7 @@
   <div class="md:flex md:items-center mb-6 mt-4">
     <div class="">
       <input v-model="checkbox" id="public" type="checkbox">
-      <label class="block text-gray-600 font-bold
+      <label display: inline-block; class="block text-gray-600 font-bold
       md:text-right mb-1 md:mb-0 pr-4" for="public">
         Public
       </label>
